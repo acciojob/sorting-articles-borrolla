eb73acdf0a8efb60e44684f0514ac0a54648d486
shravan kumar <117439152+borrolla@users.noreply.github.com>
2025-03-15 19:48:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 //your JS code here. If required.
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here',
-	'An Old Dog'];
-bands.sort();
+
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function stripArticles(bandName) {
+    return bandName.replace(/^(The |A |An )/i, '').trim();
+}
+
+bands.sort((a, b) => {
+    const bandA = stripArticles(a);
+    const bandB = stripArticles(b);
+    return bandA.localeCompare(bandB);
+});
 
 console.log(bands);
